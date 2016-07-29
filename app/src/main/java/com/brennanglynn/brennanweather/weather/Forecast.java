2365f9a0e7d010619ff450e2949dd3ad866ad33d
BrennanGlynn <brennanglynn@u.boisestate.edu>
2016-07-29 13:20:54 -0600

    Comment to test Brennan Branch

--- app/src/main/java/com/brennanglynn/brennanweather/weather/Forecast.java
+++ app/src/main/java/com/brennanglynn/brennanweather/weather/Forecast.java
@@ -4,4 +4,6 @@ public class Forecast {
     private Current mCurrentForecast;
     private Hour[] mHourlyForecast;
     private Day[] mDailyForecast;
+
+    //This should only show up in Brennan Branch
 }
