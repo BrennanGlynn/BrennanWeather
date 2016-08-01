d7c8a211837a364864441024dacece83188f299c
BrennanGlynn <brennanglynn@u.boisestate.edu>
2016-07-31 20:47:23 -0600

    Added two new colors to ColorWheel

--- app/src/main/java/com/brennanglynn/brennanweather/ui/ColorWheel.java
+++ app/src/main/java/com/brennanglynn/brennanweather/ui/ColorWheel.java
@@ -19,8 +19,8 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
-            "#b7c0c7",  // light gray
-//        "0xFF616261"
+            "#b7c0c7", // light gray
+            "#444444"  // medium gray
     };
 
     private String mColors2[] = {
@@ -37,6 +37,7 @@ public class ColorWheel {
             "#838cc7", // lavender
             "#7d669e", // purple
             "#53bbb4", // aqua
+            "#000000"  // black
 //            "0xFF131313"
     };
 
