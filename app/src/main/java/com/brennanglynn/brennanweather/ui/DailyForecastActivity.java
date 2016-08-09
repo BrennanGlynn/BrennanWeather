2646e75665ec6634554a042652f8d4aa43269c82
BrennanGlynn <brennanglynn@u.boisestate.edu>
2016-08-09 15:06:20 -0600

    Background stays when changing to daily

--- app/src/main/java/com/brennanglynn/brennanweather/ui/DailyForecastActivity.java
+++ app/src/main/java/com/brennanglynn/brennanweather/ui/DailyForecastActivity.java
@@ -2,10 +2,12 @@ package com.brennanglynn.brennanweather.ui;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.brennanglynn.brennanweather.R;
@@ -14,14 +16,20 @@ import com.brennanglynn.brennanweather.weather.Day;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DailyForecastActivity extends ListActivity {
 
     private Day[] mDays;
+
+    @BindView(R.id.relativeLayout) RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
@@ -29,6 +37,15 @@ public class DailyForecastActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this, mDays);
         setListAdapter(adapter);
+
+        int[] mBackground = intent.getIntArrayExtra(MainActivity.BG_GRADIENT);
+
+        GradientDrawable gd = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{mBackground[0], mBackground[1]});
+        gd.setCornerRadius(0f);
+
+        mRelativeLayout.setBackground(gd);
     }
 
 
